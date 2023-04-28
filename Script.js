b472c7012a0674166d4c4623b247ae31d036ed82
SeongkookKIM <ksk8646@naver.com>
2023-04-28 11:36:13 +0900

    Note 완료

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
+const popupTitle = popupBox.querySelector("header p");
 const closeIcon = popupBox.querySelector("header i");
 const titleTag = popupBox.querySelector(".title input");
 const descTag = popupBox.querySelector(".descripttion textarea");
@@ -20,20 +21,27 @@ const months = [
   "12월",
 ];
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let isUpdate = false;
+let updateId;
 
 addBox.addEventListener("click", () => {
+  titleTag.focus();
   popupBox.classList.add("show");
 });
+
 closeIcon.addEventListener("click", () => {
+  isUpdate = false;
   titleTag.value = "";
   descTag.value = "";
+  addBtn.innerText = "Add Note";
+  popupTitle.innerText = "Add a new Note";
   popupBox.classList.remove("show");
 });
 
 function showNotes() {
   document.querySelectorAll(".note").forEach((note) => note.remove());
 
-  notes.reverse().forEach((note) => {
+  notes.forEach((note, index) => {
     let liTag = `
     <li class="note">
     <div class="details">
@@ -47,11 +55,11 @@ function showNotes() {
       <div class="settings">
         <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
         <ol class="menu">
-        <li>
+        <li onclick="updateNote(${index},'${note.title}','${note.description}')">
           <i class="uil uil-pen"></i>
           Edit
         </li>
-        <li>
+        <li onclick="deleteNote(${index})">
           <i class="uil uil-trash"></i>
           Delete
         </li>
@@ -74,9 +82,34 @@ function showMenu(elem) {
   });
 }
 
+function deleteNote(noteId) {
+  let confirmDel = confirm("삭제하시겠습니까?");
+  if (!confirmDel) return;
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
+}
+
+function updateNote(noteId, title, desc) {
+  isUpdate = true;
+  updateId = noteId;
+  addBox.click();
+  titleTag.value = title;
+  descTag.value = desc;
+  addBtn.innerText = "update Note";
+  popupTitle.innerText = "update a Note";
+}
+
 addBtn.addEventListener("click", () => {
   let noteTitle = titleTag.value;
   let noteDesc = descTag.value;
+  let confirmUpdate = false;
+
+  if (!confirmUpdate) {
+    confirm("수정하시겠습니까?");
+  } else {
+    return;
+  }
 
   if (noteTitle || noteDesc) {
     let dateObj = new Date();
@@ -90,7 +123,13 @@ addBtn.addEventListener("click", () => {
       date: `${year}년 ${month} ${day}일`,
     };
 
-    notes.push(noteInfo);
+    if (!isUpdate) {
+      notes.push(noteInfo);
+    } else {
+      isUpdate = false;
+      notes[updateId] = noteInfo;
+    }
+    // notes.push(noteInfo);
     localStorage.setItem("notes", JSON.stringify(notes));
     closeIcon.click();
     showNotes();
